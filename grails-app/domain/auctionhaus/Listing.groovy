4fb450cb4b1d8374f95532ffe450d6648b49fc97
unknown <user@user-HP.(none)>
2012-02-15 18:07:11 -0600

    Sabins Changes

--- grails-app/domain/auctionhaus/Listing.groovy
+++ grails-app/domain/auctionhaus/Listing.groovy
@@ -2,22 +2,30 @@ package auctionhaus
 
 class Listing {
     String name
-    Date dateEnded
-    BigDecimal priceStarted
-    Customer winner
+    Date endDateTime
+    BigDecimal startingPrice
+    String description
     Customer seller
+    Customer winner
 
     static hasMany = [biddings: Bidding]
     static belongsTo = [seller: Customer]
 
     static transients = ['winningBidPrice']
 
-
     static constraints = {
+        // L1 and L6 name cannot be blank and needs to be less than 64 characters
+        name(nullable:false, blank:false, size: 1..64)
+        //L4 description can be blank but cannot be more than 256 characters
+        description(nullable:true, blank: true, size:1..256)
+        //L1 and L5 end date cannot be null. It needs to be a date sometime in the future.
+        endDateTime(nullable:false,validator:{it > new Date()}  )
+        //L1 starting Price cannot be null. Probably should not be negative
+        startingPrice(nullable: false, validator: {it > 0.00})
+        //L3 Listing is required to have a seller.
+        seller (nullable: false)
         winner (nullable: true)
     }
-
-
 
     BigDecimal getWinningBidPrice(){
         def winningBidPrice
@@ -34,9 +42,6 @@ class Listing {
             else if (priceStarted){priceStarted}
         else {0.0}
     }
-
-
-
 
 }
 
--- test/unit/auctionhaus/ListingTests.groovy
+++ test/unit/auctionhaus/ListingTests.groovy
@@ -1,9 +1,8 @@
 package auctionhaus
 
-
-
 import grails.test.mixin.*
 import org.junit.*
+import org.apache.commons.io.filefilter.FalseFileFilter
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
@@ -11,7 +10,82 @@ import org.junit.*
 @TestFor(Listing)
 class ListingTests {
 
-    void testSomething() {
-       fail "Implement me"
+    def listing
+    Customer customer = new Customer(email: 'ujjwal@joshi.com', password: 'testPassword', dateCreated: new Date())
+    
+    protected void setUp()
+    {
+        super.setUp()
     }
+    
+    void testNameIsRequired()
+    {
+       listing = new Listing(endDateTime: new Date() + 5, startingPrice: 40.99, description: 'Cannon Camera', seller: customer )
+       listing.validate()
+       assert 1 == listing.errors.fieldErrorCount
+       assert 'nullable' == listing.errors['name'].code
+    }
+    void testNameIsNotBlank()
+    {
+        listing = new Listing(name: '' ,endDateTime: new Date() + 5, startingPrice: 40.99, description: 'Cannon Camera', seller: customer )
+        listing.validate()
+        assert 1 == listing.errors.fieldErrorCount
+        assert 'blank' == listing.errors['name'].code
+    }
+    void testLengthOfName()
+    {
+        listing = new Listing(name: 'sdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdadasdasdasdasda' ,endDateTime: new Date() + 5, startingPrice: 40.99, description: 'Cannon Camera', seller: customer )
+        listing.validate()
+        assert 1 == listing.errors.fieldErrorCount
+        assert 'size.toobig' == listing.errors['name'].code
+    }
+    
+    void testDateNotNull()
+    {
+        listing = new Listing(name: 'Cannon' , startingPrice: 40.99, description: 'Cannon Camera', seller: customer )
+        listing.validate()
+        assert 1 == listing.errors.fieldErrorCount
+        assert 'nullable' == listing.errors['endDateTime'].code
+    }
+
+    void testDateIsInFuture()
+    {
+        listing = new Listing(name: 'Cannon' ,endDateTime: new Date() -2, startingPrice: 40.99, description: 'Cannon Camera', seller: customer )
+        listing.validate()
+        assert 1 == listing.errors.fieldErrorCount
+        assert 'validator.invalid' == listing.errors['endDateTime'].code
+    }
+
+    void testStartingPriceIsRequired()
+    {
+        listing = new Listing(name: 'Cannon' ,endDateTime: new Date() + 2, description: 'Cannon Camera', seller: customer )
+        listing.validate()
+        assert 1 == listing.errors.fieldErrorCount
+        assert 'nullable' == listing.errors['startingPrice'].code
+    }
+
+    void testStartingPriceIsValid()
+    {
+        listing = new Listing(name: 'Cannon' ,endDateTime: new Date() + 2, startingPrice: -0.99, description: 'Cannon Camera', seller: customer )
+        listing.validate()
+        assert 1 == listing.errors.fieldErrorCount
+        assert 'validator.invalid' == listing.errors['startingPrice'].code
+    }
+
+    void testSellerIsRequired()
+    {
+        listing = new Listing(name: 'Cannon' ,endDateTime: new Date() + 2, startingPrice: 0.99, description: 'Cannon Camera')
+        listing.validate()
+        assert 1 == listing.errors.fieldErrorCount
+        assert 'nullable' == listing.errors['seller'].code
+    }
+    void testLengthOfDescription()
+    {
+        listing = new Listing(name: 'Cannon' ,endDateTime: new Date() + 2, startingPrice: 0.99, description: 'qsdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdadasdasdasdasdasdasdasdasdasdasdasdasdasasdasdasdasdasdasdasdasdadasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdadsdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdadasdasdasdas', seller: customer)
+        listing.validate()
+        assert 1 == listing.errors.fieldErrorCount
+        assert 'size.toobig' == listing.errors['description'].code
+    }
+
 }
+
